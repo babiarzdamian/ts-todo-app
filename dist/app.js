faa57478e894aaca731ef02010e21fe49a5b28c7
Damian Babiarz <babiarz_damian@wp.pl>
2022-03-16 08:56:55 +0100

    feat: add creating tasks functionality

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,9 @@
+const taskNameInputElement = document.querySelector('#name');
+const addButtonElement = document.querySelector('button');
 const tasksContainerElement = document.querySelector('.tasks');
 const tasks = ['take trash out', 'feed the cat', 'go to gym'];
 const render = () => {
+    tasksContainerElement.innerHTML = '';
     tasks.forEach((task) => {
         const taskElement = document.createElement('li');
         taskElement.innerText = task;
@@ -8,4 +11,13 @@ const render = () => {
         tasksContainerElement.appendChild(taskElement);
     });
 };
+const addTask = (task) => {
+    tasks.push(task);
+};
+addButtonElement.addEventListener('click', (e) => {
+    e.preventDefault();
+    addTask(taskNameInputElement.value);
+    taskNameInputElement.value = '';
+    render();
+});
 render();
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
+const taskNameInputElement: HTMLInputElement = document.querySelector('#name');
+const addButtonElement: HTMLButtonElement = document.querySelector('button');
 const tasksContainerElement: HTMLElement = document.querySelector('.tasks');
 
 const tasks: string[] = ['take trash out', 'feed the cat', 'go to gym'];
 
 const render = () => {
+    tasksContainerElement.innerHTML = '';
     tasks.forEach((task) => {
         const taskElement: HTMLElement = document.createElement('li');
         taskElement.innerText = task;
@@ -12,5 +15,17 @@ const render = () => {
         tasksContainerElement.appendChild(taskElement);
     });
 };
+
+const addTask = (task: string) => {
+    tasks.push(task);
+};
+
+addButtonElement.addEventListener('click', (e) => {
+    e.preventDefault();
+    addTask(taskNameInputElement.value);
+    taskNameInputElement.value = '';
+
+    render();
+});
 
 render();
